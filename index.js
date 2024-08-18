2d9033372fd12b4ceda502e6221d6e237da191e7
Manoranjan-parida247 <manoranjanparida247@gmail.com>
2024-08-18 16:06:27 +0530

    violate DRY rule

--- index.js
+++ index.js
@@ -2,37 +2,37 @@ const randomNumber1 = Math.trunc(Math.random() * 6) + 1;
 const randomNumber2 = Math.trunc(Math.random() * 6) + 1;
 console.log(randomNumber1);
 console.log(randomNumber2);
-// if (randomNumber1 === 1) {
-//   document.querySelector(".img1").setAttribute("src", "./images/dice1.png");
-// } else if (randomNumber1 === 2) {
-//   document.querySelector(".img1").setAttribute("src", "./images/dice2.png");
-// } else if (randomNumber1 === 3) {
-//   document.querySelector(".img1").setAttribute("src", "./images/dice3.png");
-// } else if (randomNumber1 === 4) {
-//   document.querySelector(".img1").setAttribute("src", "./images/dice4.png");
-// } else if (randomNumber1 === 5) {
-//   document.querySelector(".img1").setAttribute("src", "./images/dice5.png");
-// } else if (randomNumber1 === 6) {
-//   document.querySelector(".img1").setAttribute("src", "./images/dice6.png");
-// }
+if (randomNumber1 === 1) {
+  document.querySelector(".img1").setAttribute("src", "./images/dice1.png");
+} else if (randomNumber1 === 2) {
+  document.querySelector(".img1").setAttribute("src", "./images/dice2.png");
+} else if (randomNumber1 === 3) {
+  document.querySelector(".img1").setAttribute("src", "./images/dice3.png");
+} else if (randomNumber1 === 4) {
+  document.querySelector(".img1").setAttribute("src", "./images/dice4.png");
+} else if (randomNumber1 === 5) {
+  document.querySelector(".img1").setAttribute("src", "./images/dice5.png");
+} else if (randomNumber1 === 6) {
+  document.querySelector(".img1").setAttribute("src", "./images/dice6.png");
+}
 
 
-// if (randomNumber2 === 1) {
-//     document.querySelector(".img2").setAttribute("src", "./images/dice1.png");
-//   } else if (randomNumber2 === 2) {
-//     document.querySelector(".img2").setAttribute("src", "./images/dice2.png");
-//   } else if (randomNumber2 === 3) {
-//     document.querySelector(".img2").setAttribute("src", "./images/dice3.png");
-//   } else if (randomNumber2 === 4) {
-//     document.querySelector(".img2").setAttribute("src", "./images/dice4.png");
-//   } else if (randomNumber2 === 5) {
-//     document.querySelector(".img2").setAttribute("src", "./images/dice5.png");
-//   } else if (randomNumber2 === 6) {
-//     document.querySelector(".img2").setAttribute("src", "./images/dice6.png");
-//   }
+if (randomNumber2 === 1) {
+    document.querySelector(".img2").setAttribute("src", "./images/dice1.png");
+  } else if (randomNumber2 === 2) {
+    document.querySelector(".img2").setAttribute("src", "./images/dice2.png");
+  } else if (randomNumber2 === 3) {
+    document.querySelector(".img2").setAttribute("src", "./images/dice3.png");
+  } else if (randomNumber2 === 4) {
+    document.querySelector(".img2").setAttribute("src", "./images/dice4.png");
+  } else if (randomNumber2 === 5) {
+    document.querySelector(".img2").setAttribute("src", "./images/dice5.png");
+  } else if (randomNumber2 === 6) {
+    document.querySelector(".img2").setAttribute("src", "./images/dice6.png");
+  }
 
-document.querySelector(".img1").setAttribute("src", `./images/dice${randomNumber1}.png`);
-document.querySelector(".img2").setAttribute("src", `./images/dice${randomNumber2}.png`);
+// document.querySelector(".img1").setAttribute("src", `./images/dice${randomNumber1}.png`);
+// document.querySelector(".img2").setAttribute("src", `./images/dice${randomNumber2}.png`);
   
 
   if(randomNumber1 > randomNumber2){
